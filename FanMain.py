25eac6ea596c7cea490e468e14734e83f86ab87e
jayceee1207 <ablayjc7@gmail.com>
2023-07-03 10:42:22 +0800

    display fan 1

--- FanMain.py
+++ FanMain.py
@@ -4,10 +4,16 @@
 #July 1, 2023
 
 #import fan
-from fan import Fan
+from Fan import Fan
 
 #create object
+fan1 = Fan()
+fan2 = Fan()
 
 #display fan 1
+fan1.set_speed()
+fan1.set_radius()
+fan1.set_color()
+fan1.set_power()
 
 #display fan 2
