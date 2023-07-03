6087ff32cfb59843358162554ca84b9984e35bb2
jayceee1207 <ablayjc7@gmail.com>
2023-07-03 09:48:10 +0800

    PSEUDOCODE for Fan Class

---
+++ Fan.py
@@ -0,0 +1,24 @@
+#create a class Fan
+
+#create constructor (default values)
+
+#create setters
+
+#create set speed
+
+#create set radius
+
+#create set color
+
+#create set power
+
+
+#create getters methods
+
+#create get speed
+
+#create get radius
+
+#create get color
+
+#create get power
