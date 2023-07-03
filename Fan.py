963d62ca0f9c79ade0f22bde386d75a112ae776b
jayceee1207 <ablayjc7@gmail.com>
2023-07-03 10:19:13 +0800

    Setter for speed

--- Fan.py
+++ Fan.py
@@ -1,15 +1,22 @@
 #create a class Fan
 class Fan:
 #create constructor (default values)
-    def __init__(self,speed = 'SLOW', radius = 5, color = 'blue', on = 0):
+    def __init__(self,speed = 'Slow', radius = 5, color = 'blue', on = 0):
         self.__speed = speed
         self.__radius = radius
         self.__color = color
         self.__on = on
 
 #create setters
-
 #create set speed
+    def set_speed(self):
+            self.__speed = int(input('Set the fan speed: '))
+            if self.__speed == 1:
+                self.__speed = 'Slow'
+            elif self.__speed == 2:
+                self.__speed = 'Medium'
+            elif self.__speed == 3:
+                self.__speed = 'Fast'
 
 #create set radius
 
