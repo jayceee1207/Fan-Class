6cdaea7c3c745a740b7e4f05f768b172b99d8d1a
jayceee1207 <ablayjc7@gmail.com>
2023-07-03 10:22:57 +0800

    Setter for radius

--- Fan.py
+++ Fan.py
@@ -10,7 +10,7 @@ class Fan:
 #create setters
 #create set speed
     def set_speed(self):
-            self.__speed = int(input('Set the fan speed: '))
+            self.__speed = int(input('Enter fan speed: '))
             if self.__speed == 1:
                 self.__speed = 'Slow'
             elif self.__speed == 2:
@@ -19,8 +19,12 @@ class Fan:
                 self.__speed = 'Fast'
 
 #create set radius
+    def set_radius(self):
+        self.__radius = float(input('Enter fan radius: '))
 
 #create set color
+    def set_color(self):
+        self.__color = str(input('Enter fan color: '))
 
 #create set power
 
